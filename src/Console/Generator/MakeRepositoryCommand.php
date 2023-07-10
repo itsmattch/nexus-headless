fdd90ec624a294e6c6619449a3ba11a2cc2d62da
itsmattch <hello@itsmattch.com>
2023-07-10 16:08:05 +0200

    Implement repository generator command

---
+++ src/Console/Generator/MakeRepositoryCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Itsmattch\NexusBridge\Console\Generator;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MakeRepositoryCommand extends GeneratorCommand
+{
+    protected $name = 'nexus:make:repository';
+
+    protected $description = 'Create new repository';
+
+    protected $type = 'Repository';
+
+    protected function getStub(): string
+    {
+        return __DIR__ . '/stubs/repository.php.stub';
+    }
+
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return $rootNamespace . '\Nexus\Repositories';
+    }
+}
--- src/NexusServiceProvider.php
+++ src/NexusServiceProvider.php
@@ -5,21 +5,21 @@ namespace Itsmattch\NexusBridge;
 use Illuminate\Support\ServiceProvider;
 use Itsmattch\NexusBridge\Console\Generator\MakeActionCommand;
 use Itsmattch\NexusBridge\Console\Generator\MakeModelCommand;
+use Itsmattch\NexusBridge\Console\Generator\MakeRepositoryCommand;
 use Itsmattch\NexusBridge\Console\Generator\MakeResourceCommand;
 
 class NexusServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-    }
+    public function register() {}
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->commands([
-                MakeResourceCommand::class,
                 MakeActionCommand::class,
                 MakeModelCommand::class,
+                MakeResourceCommand::class,
+                MakeRepositoryCommand::class,
             ]);
         }
     }
