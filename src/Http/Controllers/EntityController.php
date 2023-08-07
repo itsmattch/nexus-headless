010705c481253c1db8c5454d0ac3992ac37070d7
itsmattch <hello@itsmattch.com>
2023-08-07 21:47:01 +0200

    Entity endpoint changes

--- src/Http/Controllers/EntityController.php
+++ src/Http/Controllers/EntityController.php
@@ -3,29 +3,33 @@
 namespace Itsmattch\NexusHeadless\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use Itsmattch\NexusHeadless\Http\Requests\StoreEntityRequest;
 use Itsmattch\NexusHeadless\Models\Entity;
 
 class EntityController extends Controller
 {
-    public function index(): Collection
+    public function index(): JsonResponse
     {
-        return Entity::all();
+        return response()->json(Entity::all());
     }
 
-    public function store(StoreEntityRequest $request)
+    public function store(StoreEntityRequest $request): JsonResponse
     {
-        return Entity::create($request->all());
+        $entity = Entity::create($request->all());
+        return response()->json($entity);
     }
 
-    public function show(Entity $entity): Entity
+    public function show(Entity $entity): JsonResponse
     {
-        return $entity;
+        return response()->json($entity);
     }
 
-    public function destroy(Entity $entity): void
+    public function destroy(int $entity): Response
     {
-        $entity->delete();
+        Entity::destroy($entity);
+
+        return response()->noContent();
     }
 }
